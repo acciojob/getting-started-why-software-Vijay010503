b7dc89cf0c2387ec8c0969264cf1d4951d9a92eb
Vijay010503 <103632109+Vijay010503@users.noreply.github.com>
2025-01-12 22:36:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code herehttps://www.svgrepo.com/show/345221/three-dots.svg
 https://www.svgrepo.com/show/345221/three-dots.svg
+() => { 
+  cy.visit(baseUrl + "/main.html"); 
+  cy.get("div").then(span => { 
+    const text = span.text().trim(); 
+    expect(text).to.eq('Welcome to the world of Web development'); 
+  }); 
+}
